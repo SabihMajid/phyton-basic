efdfb7ca47983eeae7d13d30bd0c901e0ce5cbf9
SabihMajid <153662708+SabihMajid@users.noreply.github.com>
2024-07-21 03:55:40 +0500

    Add files via upload

---
+++ Firststep.py
@@ -0,0 +1,83 @@
+# First Step
+print("Sabih is my name.", "my age is 16")
+
+
+
+# Varible in phyton
+Name = "Sabih"
+Age = 16
+Class = 9
+
+
+
+# variable ko hum aik dosra variable bana kr os ky andr bhi phela variable bhi store krwa skty hai.
+# is tarah sy.
+Age2 = Age
+print(Age2)
+
+
+# in variable ko alag alag tareeky sy print krwa skty hai .
+
+# phela tareeka
+print(Name)
+
+# dosra tareeka
+print("My name is :", Name)
+
+
+
+# type in phyton type hum 5 treeky ki use krty hai
+# 1 string, 2 boolean, 3 integers ,4 None, 5 Float 
+
+
+
+# 1
+# STRING DEFINATION
+# String me hum koi bhi name ya sentence ya phir message wagera likh skty hai. jo ky double couse ky andr likhty ha 
+# :Example
+Message = ("Soda ly aoo...") 
+
+# 2
+# BOOLEAN DEFINATION
+# Boolean me hum true or flase likh skty hai or True OR Falsa ka first letter capital hota hai humesha . :Example
+Name1 = True
+
+# 3
+# INTEGERS DEfiNATION 
+# intigers hum coding me INt parhty hai or int me hum numbers print krwa skty hai or number negative hoo yaa positive hooo yaa zero kesa bhi number hoo : Example 
+Bankbalance = 22
+
+# 4
+# FLOAT DEFINATION
+# FLoat me hum decimal number rakh skty hai : Example
+Price = 25.67
+
+# 5
+# NONE DEFINATION
+# isme humary pass kuch bhi nh hota ye None hi hoti hai :Example
+age1  = None
+
+
+
+# ALL TYPE PRINT
+print(type(Message))
+print(type(Name1))
+print(type(Bankbalance))
+print(type(Price))
+print(type(age1))
+
+
+# OPERATORS
+
+# ARITHEMETIC OPERATOR
+# IS OPERATORS ME  SUM '+' , SUBTRACT '-' , MULTIPLY '*' , DIVIDE '/' , YE HUMY POWER BTA TA HAI '**' , YE HUMY REMAINDER DETA HAI JO DIVIDE KRNY PR ATA HAI '**' , '%' .  '%' . 
+
+a = 2
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)  #remainder
+print(a**b) #a^b
